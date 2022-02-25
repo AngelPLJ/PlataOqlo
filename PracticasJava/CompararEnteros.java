de24b2cf01c5d6172aa776ddcbcc71577bb36e1b
AngelPLJ <jponcel1401@alumno.ipn.mx>
2022-02-24 20:13:21 -0600

    Practicas en java

---
+++ PracticasJava/CompararEnteros.java
@@ -0,0 +1,14 @@
+package PracticasJava;
+import java.util.*;
+
+public class CompararEnteros {
+    public static void main(String[] args)
+    {
+        realizarComparacion();
+    }
+
+    private static void realizarComparacion()
+    {
+        Scanner entradaTeclado = new Scanner(System.in);
+    }
+}
